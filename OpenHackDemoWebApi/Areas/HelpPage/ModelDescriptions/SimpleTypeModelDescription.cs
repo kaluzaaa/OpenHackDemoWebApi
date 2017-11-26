a89d498a547eaab0795f1d564c7a2b4ab9640da1
Lukasz Kaluzny <lukasz@kaluzny.pro>
2017-11-26 16:10:15 +0000

    Add app

---
+++ OpenHackDemoWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OpenHackDemoWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ OpenHackDemoWebApi/Controllers/PerformanceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OpenHackDemoWebApi.Controllers
+{
+    public class PerformanceController : ApiController
+    {
+        // GET api/values
+        public int Get()
+        {
+            int cpuUsage = 75;
+            int time = 100;
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(ConsumeCPU.CPUKill));
+                t.Start(cpuUsage);
+                threads.Add(t);
+            }
+            Thread.Sleep(time);
+            foreach (var t in threads)
+            {
+                t.Abort();
+            }
+            return 1;
+        }
+    }
+
+    static class ConsumeCPU
+    {
+        public static void CPUKill(object cpuUsage)
+        {
+            Parallel.For(0, 1, new Action<int>((int i) =>
+            {
+                Stopwatch watch = new Stopwatch();
+                watch.Start();
+                while (true)
+                {
+                    if (watch.ElapsedMilliseconds > (int)cpuUsage)
+                    {
+                        Thread.Sleep(100 - (int)cpuUsage);
+                        watch.Reset();
+                        watch.Start();
+                    }
+                }
+            }));
+
+        }
+    }
+}
